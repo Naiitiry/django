cb7ff8cda653f653373bb80e79f94d937aa587a5
Naiitiry <danchukroman@gmail.com>
2024-11-12 13:53:11 -0300

    bucles

--- plantillas/plantillas/views.py
+++ plantillas/plantillas/views.py
@@ -4,5 +4,6 @@ def simple(request):
     return render(request, "simple.html", {})
 
 def dinamico(request,name):
-    context = {'name':name}
+    categories = ['code','design','marketing']
+    context = {'name':name,'categories':categories}
     return render(request,'dinamico.html',context)
