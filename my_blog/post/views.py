9cf1fe00c4ee62af820e86273e4783c080454431
Naiitiry <danchukroman@gmail.com>
2024-11-16 00:29:21 -0300

    consultas sql desde view

--- my_blog/post/views.py
+++ my_blog/post/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Author,Entry
 
-# Create your views here.
+def queries(request):
+    #Obtener todos los elementos + limite
+    authors = Author.objects.all()[:10]
+    #Si quiero sin limites saco el [:10]
+
+    #Obtener 10 elementos saltando los 5 primeros
+    offsets = Author.objects.all()[5:10]
+
+    #Se puede aplicar filtros:
+    #filtered = Author.objects.filter(email='dustinrandolph@example.net')
+    #Luego ponerlo en render, dentro de las llaves. Clase 22.
+
+    #Obtener un único objeto
+    author = Author.objects.get(id=1)
+
+    #Listado de todas las entradas.
+    #entrys = Entry.objects.all()
+
+    return render(request,'post/queries.html',{'authors':authors,'offsets':offsets})
